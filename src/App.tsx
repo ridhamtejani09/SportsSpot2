d8164f8b4e55ffd7b97211a20d074473f336d3a5
ridham tejani <rydhampatel09@gmail.com>
2025-05-23 11:47:49 +0530

    solving auth error

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthContextProvider } from "./context/AuthContext";
 import Index from "./pages/Index";
 import BookVenue from "./pages/BookVenue";
 import TeamHub from "./pages/TeamHub";
@@ -26,7 +25,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <AuthProvider>
+      <AuthContextProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
@@ -55,7 +54,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
-      </AuthProvider>
+      </AuthContextProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
